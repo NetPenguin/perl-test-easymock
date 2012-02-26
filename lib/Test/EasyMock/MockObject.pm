5ed09ebe69bdd7bfaa720e768b7acf1ff3b558ea
keita.iseki <keita.iseki@gmail.com>
2012-02-27 08:42:04 +0900

    create_mock の引数として、モック対象オブジェクトのクラス名を指定可能にしました。
    * create_mock に引数を追加
    * モックオブジェクトの isa メソッドをオーバーライドし、対象としているクラス名かどうかを返す

--- lib/Test/EasyMock/MockObject.pm
+++ lib/Test/EasyMock/MockObject.pm
@@ -2,6 +2,19 @@ package Test::EasyMock::MockObject;
 use strict;
 use warnings;
 
+# Override
+sub isa {
+    # TODO: expect 時の引数に eq や not(eq(...)) を使えるようになり次第
+    #       and_stub_return として定義する
+    my ($self, $module) = @_;
+    return unless $module;
+
+    my $self_module = $self->{_control}->{_module};
+    return unless $self_module;
+
+    return $module eq $self_module;
+}
+
 sub AUTOLOAD {
     our $AUTOLOAD;
     my $self = shift;
