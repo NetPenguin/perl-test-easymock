0338dce193d38c80cb64a14a0e4f93230f572fc1
keita.iseki <keita.iseki@gmail.com>
2012-03-07 21:17:06 +0900

    Writing a POD. Squashed commit of the following:
    commit 262240bf21ce79a01cf50b7a765d47cf173aaa32
    Author: keita.iseki <keita.iseki@gmail.com>
    Date:   Wed Mar 7 21:15:55 2012 +0900
    
        文言修正
    
    commit 8934962cdc7ba684da1df1810bb4541dedb26d70
    Author: keita.iseki <keita.iseki@gmail.com>
    Date:   Wed Mar 7 20:57:04 2012 +0900
    
        バージョン番号修正
    
    commit f67480e653efece1a8625a5a712097dfaef92d33
    Author: keita.iseki <keita.iseki@gmail.com>
    Date:   Mon Mar 5 20:18:58 2012 +0900
    
        PODを追記

--- lib/Test/EasyMock.pm
+++ lib/Test/EasyMock.pm
@@ -1,12 +1,12 @@
 package Test::EasyMock;
 use strict;
 use warnings;
-
 use Carp qw(confess);
 use Exporter qw(import);
 use Scalar::Util qw(blessed);
-
 use Test::EasyMock::MockControl;
+
+use version; our $VERSION = qv('0.01');
 
 our @EXPORT_OK = qw(
     create_mock
@@ -16,30 +16,149 @@ our @EXPORT_OK = qw(
     verify
 );
 
+=head1 NAME
+
+Test::EasyMock - A mock library which is usable easily.
+
+=head1 SYNOPSIS
+
+    use Test::EasyMock qw(
+        create_mock
+        expect
+        replay
+        verify
+        reset
+    );
+    
+    my $mock = create_mock();
+    expect($mock->foo(1))->and_scalar_return('a');
+    expect($mock->foo(2))->and_scalar_return('b');
+    replay($mock);
+    $mock->foo(1); # return 'a'
+    $mock->foo(2); # return 'b'
+    $mock->foo(3); # Unexpected method call.(A test is failed)
+    verify($mock); # verify all expectations is invoked.
+    
+    reset($mock);
+    expect($mock->foo(1, 2)->and_array_return('a', 'b');
+    expect($mock->foo({ value => 3 })->and_array_return('c');
+    replay($mock);
+    $mock->foo(1, 2); # return ('a', 'b')
+    $mock->foo({ value => 3 }); # return ('c')
+    verify($mock);
+    
+    reset($mock);
+    expect($mock->foo(1))->and_scalar_return('a');
+    expect($mock->foo(1))->and_scalar_return('b');
+    replay($mock);
+    $mock->foo(1); # return 'a'
+    $mock->foo(1); # return 'b'
+    $mock->foo(1); # Unexpected method call.(A test is failed)
+    verify($mock);
+
+=head1 DESCRIPTION
+
+This is mock library modeled on 'EasyMock' in Java.
+
+=head1 FUNCTIONS
+
+=head2 create_mock([$module_name])
+
+Creates a mock object.
+If specified the I<$module_name> then a I<isa($module_name)> method of the mock object returns true.
+
+=cut
 sub create_mock {
     my $control = Test::EasyMock::MockControl->create_control(@_);
     return $control->create_mock;
 }
 
+=head2 expect($expectation)
+
+Record a method invocation and behavior.
+
+The following example is expecting the I<foo> method invocation with I<$arguments>
+and a result of the invocation is I<123>.
+
+    expect($mock->foo($arguments))
+        ->and_scalar_return(123);
+
+And the next example is expecting the I<foo> method invocation without an argument
+and a result of the invocation is I<(1, 2, 3)>.
+
+    expect($mock->foo())
+        ->and_array_return(1, 2, 3);
+
+=head3 A list of I<and_*> methods.
+
+=over
+
+=item and_scalar_return($value)
+
+Set scalar result to the expectation.
+
+=item and_array_return(@values)
+
+Set array result to the expectation.
+
+=item and_die([$message])
+
+Set I<die> behavior to the expectation.
+
+=item and_stub_scalar_return($value)
+
+Set scalar result as a stub to the expectation.
+
+=item and_stub_array_return(@values)
+
+Set array result as a stub to the expectation.
+
+=item and_stub_die([$message])
+
+Set I<die> behavior as as stub to the expectation.
+
+=back
+
+=cut
 sub expect {
     my ($expectation) = @_;
     return __delegate(expect => ($expectation->mock, $expectation));
 }
+
+=head2 replay($mock [, $mock2 ...])
+
+Replay the mock object behaviors which is recorded by the I<expect> function.
+
+    replay($mock);
+
+=cut
 sub replay {
     __delegate(replay => $_) for @_;
 }
-sub reset {
-    __delegate(reset => $_) for @_;
-}
+
+=head2 verify($mock)
+
+Verify the mock method invocations.
+
+=cut
 sub verify {
     __delegate(verify => $_) for @_;
+}
+
+=head2 reset($mock)
+
+Reset the mock.
+
+=cut
+sub reset {
+    __delegate(reset => $_) for @_;
 }
 
 sub __delegate {
     my ($method, $mock, @args) = @_;
     my $control = __control_of($mock)
         or confess('Speocified mock is not under management');
-  return $control->$method(@args);
+    return $control->$method(@args);
 }
 
 sub __control_of {
@@ -50,3 +169,23 @@ sub __control_of {
 }
 
 1;
+__END__
+
+=head1 AUTHOR
+
+ISEKI Keita  C<< <keita.iseki+cpan at gmail.com> >>
+
+=head1 LICENCE
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=head2 EasyMock
+
+L<http://easymock.org/>
+
+It is a very wonderful library for the Java of a mock object.
+
+=cut
