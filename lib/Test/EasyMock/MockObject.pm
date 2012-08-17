61460bfb73a85de1cb7192fe57972b091c8210c6
keita.iseki <keita.iseki@gmail.com>
2012-08-17 16:38:21 +0900

    改行コードを LF に統一

--- lib/Test/EasyMock/MockObject.pm
+++ lib/Test/EasyMock/MockObject.pm
@@ -13,12 +13,12 @@ Test::EasyMock::MockObject - Mock object.
 =head2 isa($module)
 
 Mock to I<isa> method.
-    
+
 =cut
 # Override
 sub isa {
-    # TODO: expect $B;~$N0z?t$K(B eq $B$d(B not(eq(...)) $B$r;H$($k$h$&$K$J$j<!Bh(B
-    #       and_stub_return $B$H$7$FDj5A$9$k(B
+    # TODO: expect 時の引数に eq や not(eq(...)) を使えるようになり次第
+    #       and_stub_return として定義する
     my ($self, $module) = @_;
     return unless $module;
 
@@ -31,7 +31,7 @@ sub isa {
 =head2 AUTOLOAD
 
 Mock to any method.
-    
+
 =cut
 sub AUTOLOAD {
     our $AUTOLOAD;
